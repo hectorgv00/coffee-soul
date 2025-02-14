10cbc6adf76d76292fa4f2fea168a2e02ab9cdbe
hectorgv00 <hector.gv00@gmail.com>
2025-02-14 10:17:20 +0000

    new block

--- contacto.php
+++ contacto.php
@@ -1,14 +1,16 @@
 <?php include "inc/errores.php"; ?>
 <!doctype html>
 <html lang="es">
-	<head>
-		<?php include "inc/cabeza.php"; ?>
-	</head>
-	<body>
-		<?php include "modulos/cabecera/cabecera.php"; ?>
-		<?php include "modulos/contacto/contacto.php"; ?>
-		<?php include "modulos/piedepagina/piedepagina.php"; ?>
-		<?php include "modulos/modal/modal.php"; ?>
-	</body>
+
+<head>
+	<?php include "inc/cabeza.php"; ?>
+</head>
+
+<body>
+	<?php include "modulos/cabecera/cabecera.php"; ?>
+	<?php include "modulos/contacto/contacto.php"; ?>
+	<?php include "modulos/piedepagina/piedepagina.php"; ?>
+	<?php include "modulos/modal/modal.php"; ?>
+</body>
+
 </html>
-<?php include "log/registro.php"; ?>
--- tienda.php
+++ tienda.php
@@ -1,13 +1,15 @@
 <?php include "inc/errores.php"; ?>
 <!doctype html>
 <html lang="es">
-	<head>
-		<?php include "inc/cabeza.php"; ?>
-	</head>
-	<body>
-		<?php include "modulos/cabecera/cabecera.php"; ?>
-		<?php include "modulos/tienda/tienda.php"; ?>
-		<?php include "modulos/piedepagina/piedepagina.php"; ?>
-	</body>
+
+<head>
+	<?php include "inc/cabeza.php"; ?>
+</head>
+
+<body>
+	<?php include "modulos/cabecera/cabecera.php"; ?>
+	<?php include "modulos/tienda/tienda.php"; ?>
+	<?php include "modulos/piedepagina/piedepagina.php"; ?>
+</body>
+
 </html>
-<?php include "log/registro.php"; ?>
