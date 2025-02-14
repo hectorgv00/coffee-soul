4f613fee93bac1d6ed00f1e6fe0185fe9d3f4f73
hectorgv00 <hector.gv00@gmail.com>
2025-02-14 10:27:11 +0000

    cambio en relacionados

--- modulos/productosrelacionados/productosrelacionados.js
+++ modulos/productosrelacionados/productosrelacionados.js
@@ -1,27 +1,30 @@
-function productosRelacionados(){
-	fetch("../back/?tabla=productos") 											// Cargo un endpoint en el back
-	.then(function(response) { 													// Cuando obtenga respuesta
-		 return response.json(); 													// La convierto en json
-	})
-	.then(function(datos) { 
-		console.log(datos)
-		let contenedor = document.querySelector("aside")
-		datos.splice(4);
-		datos.forEach(function(dato){
-			let articulo = document.createElement("article")
-			let titulo = document.createElement("h4")
-			titulo.textContent = dato.titulo
-			let descripcion = document.createElement("p")
-			descripcion.textContent = dato.descripcion
-			articulo.appendChild(titulo)
-			articulo.appendChild(descripcion)
-			let boton = document.createElement("button")
-			boton.textContent = "Saber más"
-			articulo.appendChild(boton)
-			contenedor.appendChild(articulo)
-			
-		})
-	})
+function productosRelacionados() {
+  fetch("../back/?tabla=productos") // Cargo un endpoint en el back
+    .then(function (response) {
+      // Cuando obtenga respuesta
+      return response.json(); // La convierto en json
+    })
+    .then(function (datos) {
+      console.log(datos);
+      let contenedor = document.querySelector("aside");
+      datos.splice(4);
+      datos.forEach(function (dato) {
+        let articulo = document.createElement("article");
+        let titulo = document.createElement("h4");
+        titulo.textContent = dato.titulo;
+        let descripcion = document.createElement("p");
+        descripcion.textContent = dato.descripcion;
+        articulo.appendChild(titulo);
+        articulo.appendChild(descripcion);
+        let boton = document.createElement("button");
+        boton.textContent = "Saber más";
+        boton.addEventListener("click", function () {
+          window.location.href = `producto.php?prod=${dato.Identificador}`;
+        });
+        articulo.appendChild(boton);
+        contenedor.appendChild(articulo);
+      });
+    });
 }
 
 productosRelacionados();
